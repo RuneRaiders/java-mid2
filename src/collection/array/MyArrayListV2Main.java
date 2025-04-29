4e276713baca5d3e7509ea5a2a32418f764d6710
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-29 20:47:53 +0900

    직접 구현하는 배열 리스트2 - 동적 배열

---
+++ src/collection/array/MyArrayListV2Main.java
@@ -0,0 +1,22 @@
+package collection.array;
+
+public class MyArrayListV2Main {
+
+    public static void main(String[] args) {
+        MyArrayListV2 list = new MyArrayListV2();
+        System.out.println("== 데이터 추가 ==");
+        System.out.println(list);
+        list.add("A");
+        System.out.println(list);
+        list.add("B");
+        System.out.println(list);
+        list.add("C");
+        System.out.println(list);
+        list.add("D");
+        System.out.println(list);
+        list.add("E");
+        System.out.println(list);
+        list.add("F");
+        System.out.println(list);
+    }
+}
