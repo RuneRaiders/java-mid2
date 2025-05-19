b7353081a399b06c71ac7f00eb5aab1e2fde0fc2
RuneRaiders <hyeonseoban47@gmail.com>
2025-05-19 20:56:07 +0900

    문제와 풀이2

--- src/collection/list/test/ex2/ShoppingCartMain.java
+++ src/collection/list/test/ex2/ShoppingCartMain.java
@@ -4,6 +4,7 @@ public class ShoppingCartMain {
 
     public static void main(String[] args) {
 
+        ShoppingCart cart = new ShoppingCart();
 
         Item item1 = new Item("마늘", 2000, 2);
         Item item2 = new Item("상추", 3000, 2);
